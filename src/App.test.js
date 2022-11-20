080024586d9a529d978c93d128ead0f019f87f1f
Chandan <Chandan17ee@gmail.com>
2022-11-20 15:59:49 +0530

    feat: added ci/cd pipe line

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Render App", () => {
+  test("Match Snapshot", () => {
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
+
+  test("App To be defined", () => {
+    const { container } = render(<App />);
+    expect(container).toBeDefined();
+  });
 });
